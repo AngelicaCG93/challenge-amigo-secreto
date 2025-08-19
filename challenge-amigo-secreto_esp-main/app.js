2d9c0f6e0419b96357fc0773d6ea0c13e75d4139
AngelicaCG93 <lissangelicacg@gmail.com>
2025-08-19 15:05:56 -0600

    Actualizacipon: Se agrega función para actualizar amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,20 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let names = [];
+let amigos = [];
+
+function actualizarLista() {
+	const lista = document.getElementById('listaAmigos');
+
+	lista.innerHTML = '';
+
+	for (let i = 0; i < amigos.length; i++) {
+		const li = document.createElement('li');
+		li.textContent = amigos[i];
+		lista.appendChild(li);
+	}
+}
 
 function agregarAmigo() {
 	const input = document.getElementById('amigo'); // busca el input
 	const nombre = input.value; // toma el texto y quita espacios
 
 	if (nombre !== '') {
-		names.push(nombre); // guarda el nombre en el array
-
-		// muestra el nombre en pantalla
-		const lista = document.getElementById('listaAmigos');
-		const li = document.createElement('li');
-		li.textContent = nombre;
-		lista.appendChild(li);
-
+		amigos.push(nombre); // guarda el nombre en el array
+		actualizarLista();
 		input.value = ''; // limpia el campo
 	} else {
 		alert('Por favor, escribe un nombre.');
