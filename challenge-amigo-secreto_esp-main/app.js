015124bbe14cdfcbe628361c8c79d96d93ce418a
AngelicaCG93 <lissangelicacg@gmail.com>
2025-08-19 14:33:38 -0600

    Actualización: se agrega función para agregar amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,3 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let names = [];
+
+function agregarAmigo() {
+	const input = document.getElementById('amigo'); // busca el input
+	const nombre = input.value; // toma el texto y quita espacios
+
+	if (nombre !== '') {
+		names.push(nombre); // guarda el nombre en el array
+
+		// muestra el nombre en pantalla
+		const lista = document.getElementById('listaAmigos');
+		const li = document.createElement('li');
+		li.textContent = nombre;
+		lista.appendChild(li);
+
+		input.value = ''; // limpia el campo
+	} else {
+		alert('Por favor, escribe un nombre.');
+	}
+}
